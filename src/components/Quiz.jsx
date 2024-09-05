eeb507edeb750a3f79ea308391f239a2796a50cc
aamiryaqoob <aamiryaqoob965@gmail.com>
2024-09-05 23:27:04 +0500

    update

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react"
 import QUESTIONS from '../questions'
+import QuizImg from '../assets/quiz-complete.png';
+import QuestionTimer from "./QuestionTimer";
 
 export default function Quiz() {
     const [userAnswer, setUserAnswer] = useState([]);
 
     const activeQuestionsIndex = userAnswer.length;
+    const quizIsComplete = activeQuestionsIndex === QUESTIONS.length;
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswer((prevUserAnswers) => {
@@ -12,12 +15,22 @@ export default function Quiz() {
         });
     }
 
+    if (quizIsComplete) {
+        return <div id="summary">
+            <img src={QuizImg} alt="Trophy Icon" />
+            <h2>Quiz Completed!</h2>
+        </div>
+    }
+    const suffledAnswer = [...QUESTIONS[activeQuestionsIndex].answers];
+    suffledAnswer.sort((a, b) => Math.random() - 0.5);
+
     return (
         <div id="quiz">
             <div id="question">
+                <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)} />
                 <h2>{QUESTIONS[activeQuestionsIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionsIndex].answers.map((answer) => (
+                    {suffledAnswer.map((answer) => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
